1f6bde4ddc8bc609accbef6f34a19d57192667ef
LightLord <stevensah88@gmail.com>
2024-04-09 00:38:27 -0700

    Updates

---
+++ assets/location-CqQpgvLv.js
@@ -0,0 +1,1 @@
+import"./index-LcCS4vZo.js";navigator.geolocation.getCurrentPosition(t=>{document.getElementById("location").innerText=`Latitude: ${t.coords.latitude}, Longitude: ${t.coords.longitude}`});
