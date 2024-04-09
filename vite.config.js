70bcb3045f7c38be823a1f014e99bd53fb326801
LightLord <stevensah88@gmail.com>
2024-04-09 00:38:17 -0700

    fix: css of index.html and vite config not building all html files (bug)

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/csmassignment3/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        products: resolve(__dirname, "products.html"),
+        location: resolve(__dirname, "location.html"),
+        extra: resolve(__dirname, "extra.html"),
+        contact: resolve(__dirname, "contact.html"),
+      }
+    }
+  }
 });
